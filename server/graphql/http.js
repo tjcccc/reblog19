1f94892173cb798e1734c586563626517ddc3fed
seesaw game <tttjjjccc@gmail.com>
2019-07-10 10:50:21 +0800

    Update packages.

--- server/graphql/http.js
+++ server/graphql/http.js
@@ -4,17 +4,21 @@ const schemaConstructor = require('./schema.constructor');
 module.exports = graphqlHTTP({
   schema: schemaConstructor,
   graphiql: true,
-  formatError(error) {
+  customFormatErrorFn: error => {
     if (!error.originalError) {
       return error;
     }
+    const locations = error.locations;
     const data = error.originalError.data;
     const message = error.message || 'An error occurred.';
     const code = error.originalError.code || 500;
+    const path = error.path;
     return {
       message: message,
+      locations: locations,
       status: code,
-      data: data
+      data: data,
+      path: path
     };
   }
 })
