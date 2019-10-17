c7f7da41b6fbabf1ca660398d5eb94c8dc0b65ef
seesaw game <tttjjjccc@gmail.com>
2019-10-17 13:44:21 +0800

    Update Chronicle list style.

--- src/components/Chronicle.jsx
+++ src/components/Chronicle.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import converter from '../utilities/converter';
+import logger from '../utilities/logger';
 
 class Chronicle extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedYear: 0
+    }
   }
 
   displayName = 'Chronicle';
@@ -18,19 +22,27 @@ class Chronicle extends Component {
     return 2014;
   }
 
+  selectYear = (year) => {
+    logger.trace(year);
+    this.setState({
+      selectedYear: parseInt(year)
+    })
+  }
+
   render = () => {
     const { firstYear } = this.props;
+    const { selectedYear } = this.state;
 
     const currentYear = this.getCurrentYear();
-    const yearList = converter.getRange(firstYear, currentYear + 1);
+    const yearList = converter.getRange(firstYear, currentYear + 1).reverse();
 
     const monthList = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
 
     const months = monthList.map((month, index) => <li key={index}><a href="#">{month} (?)</a></li>);
 
     const years = yearList.map((year, index) =>
-      <div key={index}>
-        <a href='#'>{year}</a>
+      <div key={index} className={year === selectedYear ? 'reveal' : 'hide'}>
+        <button onClick={() => this.selectYear(year)}>{year} (?)</button>
         <ul>
           {months}
         </ul>
